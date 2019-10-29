18916a79b1e2795501c9dbd9e755f42ce9c9b49d
Arkadiusz Pien <arek.pien@gmail.com>
2019-10-29 20:50:30 +0100

    Update development script to run an app.

--- bin/dev/run.sh
+++ bin/dev/run.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 #Run the bash crawler and the result store in file 'result_fille_path.json' in the user directory.
 
+if [ "$#" -ne 1 ]; then
+	echo  "Usage: $(basename "$0") {number_of_pages_to_fetch}"
+	exit 1
+fi
+
 export BASH_CRAWLER_RESULT_FILE_PATH=$HOME/result_fille_path.json
 cd ../../
-sbt run
+sbt "run $1"
