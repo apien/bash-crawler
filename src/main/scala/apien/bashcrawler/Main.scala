fd4b14a836a0ec266f060f1d48094b403a111adb
Arkadiusz Pien <arek.pien@gmail.com>
2019-10-29 20:25:26 +0100

    Main class of a application.
    - The application take number of pages to fetch. It needs to be
      number greater than 0.
    - Auxiliary constructor for PageNumber which validate a provided value.

---
+++ src/main/scala/apien/bashcrawler/Main.scala
@@ -0,0 +1,51 @@
+package apien.bashcrawler
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpRequest
+import akka.stream.ActorMaterializer
+import apien.bashcrawler.domain.PageNumber
+import apien.bashcrawler.infrastructure.{BCConfig, JsonSupport, MessageFileRepository, ScrapperBaschClient}
+import apien.bashcrawler.service.BashCrawler
+import apien.bashcrawler.service.BashCrawler.Statistics
+import com.typesafe.scalalogging.LazyLogging
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
+
+object Main extends App with JsonSupport with LazyLogging {
+
+  val pageNumber = if (args.length < 1) {
+    println("Run crawler with one argument - how many first pages you want to fetch. Value not less than 1")
+    None
+  } else PageNumber(args(0))
+
+  pageNumber.foreach(run)
+
+  private def run(pageNumber: PageNumber): Unit = {
+    implicit val system: ActorSystem = ActorSystem("reactive-tweets")
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+    val config = BCConfig.loadConfig
+    val httpClient = (req: HttpRequest) => Http().singleRequest(req)
+    val client = new ScrapperBaschClient(config.bashUrl, httpClient)
+    val repository = new MessageFileRepository(config.resultFilePath)
+    val crawler = new BashCrawler(client, repository)
+
+    crawler
+      .process(pageNumber)
+      .onComplete {
+        case Success(Statistics(fetchedObjects)) =>
+          println(s"Successfully fetched $fetchedObjects messages from bash")
+          cleanResources()
+        case Failure(exception) =>
+          logger.error(s"Ups! Something gone wrong $exception")
+          cleanResources()
+      }
+
+    def cleanResources() = {
+      materializer.shutdown()
+      system.terminate()
+    }
+  }
+
+}
--- src/main/scala/apien/bashcrawler/domain/PageNumber.scala
+++ src/main/scala/apien/bashcrawler/domain/PageNumber.scala
@@ -1,7 +1,15 @@
 package apien.bashcrawler.domain
 
 /**
-  * It represents a page number for the bash.
+  * It represents a page number for the bash. Minimal page is 1.
   * @param value -.
   */
-case class PageNumber(value: Long) extends AnyVal
+case class PageNumber private (value: Long) extends AnyVal
+
+object PageNumber {
+  def apply(value: String): Option[PageNumber] = {
+    value.toLongOption
+      .filter(_ > 0)
+      .map(value => new PageNumber(value))
+  }
+}
