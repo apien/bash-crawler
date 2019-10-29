0fc4e8adf9043f7b24ac66d5bf2e6122dad76df3
Arkadiusz Pien <arek.pien@gmail.com>
2019-10-29 17:47:51 +0100

    Config keep a path to result file as a Path not just String.

--- src/main/scala/apien/bashcrawler/infrastructure/BCConfig.scala
+++ src/main/scala/apien/bashcrawler/infrastructure/BCConfig.scala
@@ -1,4 +1,6 @@
 package apien.bashcrawler.infrastructure
+import java.nio.file.Path
+
 import pureconfig._
 import pureconfig.generic.auto._
 
@@ -6,7 +8,7 @@ import pureconfig.generic.auto._
   * Bash crawler configuration.
   * @param resultFilePath Path of the file which contains the final result.
   */
-case class BCConfig(resultFilePath: String)
+case class BCConfig(resultFilePath: Path)
 
 object BCConfig {
   def loadConfig = ConfigSource.default.at("bashcrawler").loadOrThrow[BCConfig]
